f1fefd5e0055b27f2335fe44820102cf593b6b08
山本敬太 <yamamoto@altonotes.co.jp>
2022-12-21 12:05:46 +0900

    Add tests

--- src/searcher/searcher_tests.rs
+++ src/searcher/searcher_tests.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 
+#[allow(unused_imports)]
 mod searcher_tests {
     use crate::evaluator::stone_count::StoneCountEvaluator;
-    use crate::{Board, NegaAlpha};
+    use crate::{Board, NegaAlpha, points_to_str};
     use crate::searcher::alpha_beta::AlphaBeta;
     use crate::searcher::game_tree_searcher::GameTreeSearcher;
 
@@ -22,18 +23,35 @@ mod searcher_tests {
     }
 
     #[test]
-    fn second_move() {
-        test_second_move(&alpha_beta());
-        test_second_move(&nega_alpha());
+    fn black_move() {
+        test_black_move(&alpha_beta());
+        test_black_move(&nega_alpha());
     }
 
-    fn test_second_move(searcher: &dyn GameTreeSearcher) {
-        let board = Board::new_by_moves("F5");
+    fn test_black_move(searcher: &dyn GameTreeSearcher) {
+        let board = Board::new_by_moves("F5F4");
+        let result = searcher.evaluate_children(&board, 1);
+        assert_eq!(result.len(), 5);
+        assert_eq!(result[0].1, 3);
+        assert_eq!(result[1].1, 5);
+        assert_eq!(result[2].1, 3);
+        assert_eq!(result[3].1, 5);
+        assert_eq!(result[4].1, 3);
+    }
+
+    #[test]
+    fn white_move() {
+        test_white_move(&alpha_beta());
+        test_white_move(&nega_alpha());
+    }
+
+    fn test_white_move(searcher: &dyn GameTreeSearcher) {
+        let board = Board::new_by_moves("F5F4F3");
         let result = searcher.evaluate_children(&board, 1);
         assert_eq!(result.len(), 3);
-        for (_, value) in result {
-            assert_eq!(value, 0);
-        }
+        assert_eq!(result[0].1, -2);
+        assert_eq!(result[1].1, -2);
+        assert_eq!(result[2].1, 0);
     }
 
     #[test]
