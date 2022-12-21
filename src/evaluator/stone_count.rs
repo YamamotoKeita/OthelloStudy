3e5f559317b0b011f3bb09a794a0f84720a07bc6
山本敬太 <yamamoto@altonotes.co.jp>
2022-12-21 10:24:54 +0900

    Make weight of stone count dynamic

--- src/evaluator/stone_count.rs
+++ src/evaluator/stone_count.rs
@@ -1,20 +1,21 @@
 use crate::{Board, PlayerType};
 use crate::evaluator::Evaluator;
 
-const MAGNITUDE: i32 = 100;
-
-pub struct StoneCountEvaluator {}
+pub struct StoneCountEvaluator {
+    stone_weight: i32,
+}
 
 impl Evaluator for StoneCountEvaluator {
     fn evaluate(&self, board: &Board) -> i32 {
         let count1 = board.count_stones(PlayerType::First);
         let count2 = board.count_stones(PlayerType::Second);
-        (count1 as i32 - count2 as i32) * MAGNITUDE
+        (count1 as i32 - count2 as i32) * self.stone_weight
     }
 }
 
+#[allow(dead_code)]
 impl StoneCountEvaluator {
     pub fn new() -> StoneCountEvaluator {
-        StoneCountEvaluator {}
+        StoneCountEvaluator {stone_weight: 1}
     }
 }
